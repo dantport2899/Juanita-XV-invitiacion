c785720afa38f2e73ad7c75fce06d14a519740df
Dante Portales <dantport28@gmail.com>
2022-02-11 17:24:37 -0600

    avance 5pm

---
+++ js/fun.js
@@ -0,0 +1,18 @@
+'use strict'
+window.addEventListener('load', () => {  //al hacer esto ya no es necesario cargar el script al final del html
+
+    $(document).ready(function(){ 
+         
+        $('#scroll').click(function(){ 
+            $("html, body").animate({ scrollTop: 0 }, 1000); 
+            return false; 
+        });
+        
+        $('#scrollBottom').click(function(){ 
+            $("html, body").animate({ scrollTop: 9999 }, 1000); 
+            return false; 
+        }); 
+
+    });
+
+ });
